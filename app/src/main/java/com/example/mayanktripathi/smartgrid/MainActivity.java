ad00eae8d83be41cf170baf6c0b5b73ed3beec15
Mayank <mayank.trp48@gmail.com>
2017-01-20 19:54:10 +0530

    Intial comit

---
+++ app/src/main/java/com/example/mayanktripathi/smartgrid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mayanktripathi.smartgrid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
