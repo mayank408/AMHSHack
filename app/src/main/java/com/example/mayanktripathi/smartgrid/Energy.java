64f4ae84d68bbfe6a947b85b2788292c5d3d6598
Mayank <mayank.trp48@gmail.com>
2017-01-21 02:59:51 +0530

    graph created updated

--- app/src/main/java/com/example/mayanktripathi/smartgrid/Energy.java
+++ app/src/main/java/com/example/mayanktripathi/smartgrid/Energy.java
@@ -1,7 +1,10 @@
 package com.example.mayanktripathi.smartgrid;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.View;
@@ -22,6 +25,7 @@ public class Energy extends AppCompatActivity {
     CardView cd3;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,17 +37,44 @@ public class Energy extends AppCompatActivity {
 
 
         GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+        final LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
                 new DataPoint(0, 1),
                 new DataPoint(1, 5),
                 new DataPoint(2, 3)
         });
+        series.setColor(Color.BLACK);
         graph.addSeries(series);
+
+        final LineGraphSeries<DataPoint> series2 = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 3),
+                new DataPoint(2, 1.5)
+        });
+        series2.setColor(Color.BLACK);
+        graph.addSeries(series2);
+
+        final LineGraphSeries<DataPoint> series3 = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 0.5),
+                new DataPoint(1, 2),
+                new DataPoint(2, 1.25)
+        });
+        series3.setColor(Color.BLACK);
+        graph.addSeries(series3);
+
+        final LineGraphSeries<DataPoint> series4 = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 0.24),
+                new DataPoint(1, 1.2),
+                new DataPoint(2, 2)
+        });
+        series4.setColor(Color.BLACK);
+        graph.addSeries(series4);
 
 
         cd1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                series2.setColor(Color.BLUE);
+                series.setThickness(10);
                 Toast.makeText(Energy.this, "one", Toast.LENGTH_SHORT).show();
             }
         });
@@ -51,6 +82,8 @@ public class Energy extends AppCompatActivity {
         cd2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                series3.setColor(Color.BLUE);
+                series.setThickness(10);
                 Toast.makeText(Energy.this, "two", Toast.LENGTH_SHORT).show();
             }
         });
@@ -58,6 +91,8 @@ public class Energy extends AppCompatActivity {
         cd3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                series4.setColor(Color.BLUE);
+                series.setThickness(10);
                 Toast.makeText(Energy.this, "three", Toast.LENGTH_SHORT).show();
             }
         });
